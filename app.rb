1d7939c60a1a2c2835076b3a753cc98cddf1e4ed
Vikram Ramakrishnan <lord.ezar@gmail.com>
2015-07-04 20:01:18 -0400

    href counter

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'app/models/bot'
 
 filename = ENV["FILENAME"]
-Bot.new(filename).populate!
+print "How many do you want to populate with?: "
+count = gets.chomp.to_i
+puts "No problem!"
+
+print "Do you want to (1) create images or (2) count hrefs?: "
+choice = gets.chomp.to_i
+
+if choice == 1
+  puts "Starting imagemaker!"
+  Bot.new(filename).populate!(count)
+else
+  puts "Starting counter!"
+  HrefCounter.new(filename).counter(count)
+end
--- app/models/bot.rb
+++ app/models/bot.rb
@@ -5,13 +5,16 @@ require 'rest_client'
 require 'pry'
 require 'httparty'
 
-class Bot
+class HrefCounter
   attr_reader :filename
+
   def initialize(filename)
     @filename = filename
   end
 
-  def populate!
+  def counter(count)
+    has_hrefs = 0
+
     urls = Array.new
 
     puts 'bot waking up...'
@@ -25,7 +28,47 @@ class Bot
 
     puts 'Getting files and saving as htmls'
     filenames = Array.new
-    urls[0..3].each_with_index do |url, index|
+    urls[0...count].each_with_index do |url, index|
+      body = HTTParty.get(url)
+      length = body.length
+      relevant_length = length / 10
+      body = body[0..relevant_length]
+      href_count = body.scan(/href|HREF/).count
+      has_hrefs += 1 if href_count > 0
+
+      puts "this file has #{href_count} href tags"
+      puts "------"
+      puts
+    end
+    puts "Total docs with hrefs: #{has_hrefs}"
+    puts "Total docs: #{count}"
+    puts "Total Docs without hrefs: #{count - has_hrefs}"
+    puts "%: #{100 * (count - has_hrefs).to_f / count}"
+  end
+
+end
+
+class Bot
+  attr_reader :filename
+  def initialize(filename)
+    @filename = filename
+  end
+
+  def populate!(count)
+    urls = Array.new
+
+    puts 'bot waking up...'
+
+    puts 'Reading from CSV'
+    CSV.foreach(filename, headers: true) do |row|
+      http_location = row["http_location"]
+      urls << http_location
+    end
+    puts 'Reading CSV complete'
+
+    puts 'Getting files and saving as htmls'
+    filenames = Array.new
+    urls[0..count].each_with_index do |url, index|
       body = HTTParty.get(url)
       length = body.length
       relevant_length = length / 10
@@ -36,6 +79,12 @@ class Bot
       File.open(filename, 'w') do |file|
         file.write(body)
       end
+      puts "Saved file no. #{index}"
+      href_count = body.scan(/href|HREF/).count
+      puts "this file has #{href_count} href tags"
+      puts "------"
+      puts
+
     end
 
     Capybara.configure do |config|
@@ -53,7 +102,7 @@ class Bot
     puts 'Logging in'
 
     filenames.each_with_index do |filename, index|
-      puts "visiting #{filename}"
+      puts "#{index}: visiting #{filename}"
       s.visit filename
       s.driver.save_screenshot("temp/images/
         file#{index}.png")
